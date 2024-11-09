bf790a33e7efe8fd978aff46d1cd17d2cf224aba
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-11-09 14:45:21 +0530

    first

--- index.js
+++ index.js
@@ -1,64 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const ACTIONS = require("./Action");
-// require("dotenv").config();
-
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// const userSocketMap = {};
-// function getAllConnectedClients(roomId) {
-//   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-//     (socketId) => {
-//       return {
-//         socketId,
-//         username: userSocketMap[socketId],
-//       };
-//     }
-//   );
-// }
-
-// io.on("connection", (socket) => {
-//   console.log("socket connected", socket.id);
-//   socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-//     userSocketMap[socket.id] = username;
-//     socket.join(roomId);
-//     const clients = getAllConnectedClients(roomId);
-//     clients.forEach(({ socketId }) => {
-//       io.to(socketId).emit(ACTIONS.JOINED, {
-//         clients,
-//         username,
-//         socketId: socket.id,
-//       });
-//     });
-//   });
-
-//   socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-//     socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-//   });
-
-//   socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-//     io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-//   });
-
-//   socket.on("disconnecting", () => {
-//     const rooms = [...socket.rooms];
-//     rooms.forEach((roomId) => {
-//       socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-//         socketId: socket.id,
-//         username: userSocketMap[socket.id],
-//       });
-//     });
-//     delete userSocketMap[socket.id];
-//     socket.leave();
-//   });
-// });
-
-// const PORT = process.env.PORT || 8000;
-// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -84,7 +23,6 @@ function getAllConnectedClients(roomId) {
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
 
-  // Handle user joining a room
   socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
     userSocketMap[socket.id] = username;
     socket.join(roomId);
@@ -99,17 +37,14 @@ io.on("connection", (socket) => {
     });
   });
 
-  // Handle code change (existing functionality)
   socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
     socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
   });
 
-  // Sync code between clients
   socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
     io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
   });
 
-  // Handle chat message send
   socket.on("SEND_MESSAGE", ({ roomId, message }) => {
     const username = userSocketMap[socket.id];
     if (username) {
@@ -118,12 +53,10 @@ io.on("connection", (socket) => {
         message,
         timestamp: new Date().toISOString(),
       };
-      // Broadcast message to all clients in the same room
       io.in(roomId).emit("RECEIVE_MESSAGE", chatMessage);
     }
   });
 
-  // Handle user disconnecting
   socket.on("disconnecting", () => {
     const rooms = [...socket.rooms];
     rooms.forEach((roomId) => {
